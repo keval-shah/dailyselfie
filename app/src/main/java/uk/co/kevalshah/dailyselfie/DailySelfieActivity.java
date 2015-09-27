a6299621b322256f0962cee8617c0df3f851459e
Keval Shah <mail@kevalshah.co.uk>
2015-09-27 18:41:11 +0100

    Android Studio generate project. +Added icons for camera menu action. +Added dummy behaviour for on menu icon click for camera.

---
+++ app/src/main/java/uk/co/kevalshah/dailyselfie/DailySelfieActivity.java
@@ -0,0 +1,39 @@
+package uk.co.kevalshah.dailyselfie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class DailySelfieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_selfie);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_daily_selfie, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_camera) {
+            Toast.makeText(getApplicationContext(), "Clicked camera icon", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
